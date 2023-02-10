79e867e01318efd8292917056ba6b75af37ad952
AriGamesLP <the.bros.85.03@gmail.com>
2023-02-10 14:51:36 +0100

    basic github login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
+import LoginButton from "../components/LoginButton";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+
+const App = () => {
+    return(<>
+        <LoginButton />
+    </>);
 }
+
+const AppProviderWrapper = ({Component, pageProps: { session, ...pageProps },}:AppProps<{ session: Session }>) => {
+    return(
+        <SessionProvider session={session}>
+            <App/>
+        </SessionProvider>
+    );
+}
+
+export default AppProviderWrapper;
