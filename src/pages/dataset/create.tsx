76d93f2861eb9836a930e32ca4ee5549e62a1df1
AriGamesLP <the.bros.85.03@gmail.com>
2023-02-11 20:50:26 +0100

    Dropdown Components

--- src/pages/dataset/create.tsx
+++ src/pages/dataset/create.tsx
@@ -1,5 +1,32 @@
+import Dropdown from "@/components/nav/inputs/Dropdown";
+import DropdownDetailed from "@/components/nav/inputs/DropdownDetailed";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import ContrastIcon from "@mui/icons-material/Contrast";
+
 const CreateDataset = () => {
-    return(<></>);
-}
+  return (
+    <>
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <DropdownDetailed
+          options={[
+            {
+              title: "Fast Bounding Boxes",
+              description:
+                "Faster to create, but results in looser bounding Boxes",
+              icon: AlarmIcon
+            },
+            {
+              title: "Tight Bounding Boxes",
+              description:
+                "Slower to create, but results in tighter bounding Boxes",
+              icon: ContrastIcon
+            }
+          ]}
+        />
+        <Dropdown options={[{ title: "Option 1" }, { title: "Option 2" }]} />
+      </div>
+    </>
+  );
+};
 
 export default CreateDataset;
