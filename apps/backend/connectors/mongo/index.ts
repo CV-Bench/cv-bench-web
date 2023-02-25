90067aebafebcd981dbfbed3886cc6b531d3b17d
AriGamesLP <the.bros.85.03@gmail.com>
2023-02-25 18:52:26 +0100

    add mongo user connector

--- apps/backend/connectors/mongo/index.ts
+++ apps/backend/connectors/mongo/index.ts
@@ -7,7 +7,8 @@ export {
 
 import { default as Model } from "./model";
 import { default as Dataset } from "./dataset";
+import { default as User } from "./user";
 
-const Database = { Model, Dataset };
+const Database = { Model, Dataset, User };
 
 export default Database;
---
+++ apps/backend/connectors/mongo/user.ts
@@ -0,0 +1,57 @@
+import { Collection, FindCursor, InsertOneResult, ObjectId } from "mongodb";
+import {
+  AccessType,
+  CollectionName,
+  loggerTitle,
+  SessionUser,
+} from "types";
+import logger from "../../util/logger";
+import { collectionRequest, prepareCollection } from "./";
+
+prepareCollection(CollectionName.USER).then((collection) => {
+  logger.debug(
+    loggerTitle.MONGO_CLIENT,
+    `Collection Ready: ${collection.namespace}`
+  );
+});
+
+const findOne = (id: string | ObjectId) =>
+  collectionRequest<SessionUser>(CollectionName.USER, async (collection) => {
+    return collection.findOne({
+      _id: new ObjectId(id),
+    });
+  });
+
+const insert = (user: SessionUser) =>
+  collectionRequest<InsertOneResult>(
+    CollectionName.USER,
+    async (collection) => {
+      return collection.insertOne(user);
+    }
+  );
+
+const updateOne = (id: string | ObjectId, update: Partial<SessionUser>) =>
+  collectionRequest<SessionUser>(CollectionName.USER, async (collection) => {
+    return collection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      { $set: update }
+    );
+  });
+
+const deleteOne = (id: string | ObjectId) =>
+  collectionRequest<SessionUser>(CollectionName.USER, async (collection) => {
+    return collection.deleteOne({
+      _id: new ObjectId(id),
+    });
+  });
+
+const User = {
+  findOne,
+  insert,
+  updateOne,
+  deleteOne,
+};
+
+export default User;
