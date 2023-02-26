16827078c0d9e4f43a4eeca6174b0a72ae80a21a
Vova Shcherbyna <volodymyr.shcherbyna@campus.tu-berlin.de>
2023-02-26 16:45:57 +0100

    datasetConfiguration aktualisiert
    Signed-off-by: Vova Shcherbyna <volodymyr.shcherbyna@campus.tu-berlin.de>

--- packages/types/src/datasetConfiguration.ts
+++ packages/types/src/datasetConfiguration.ts
@@ -5,10 +5,9 @@ export enum ConfigurationType {
   "BLENDER",
 }
 
-export enum RandomColor {
-  NONE = "None",
-  TEMPERATURE = "Temperature",
-  PROJECTOR = "Projector",
+export enum ComputeBbox {
+  FAST = "fast",
+  TIGHT = "tight"
 }
 
 export enum CamLensUnit {
@@ -18,75 +17,67 @@ export enum CamLensUnit {
 
 //CONFIGURATION OBJECT user-editable
 export const BlenderConfigurationObject = z.object({
-  maxDistractorObjects: z.number().int().gte(0), //max number of distractor objects
 
-  numberOfObjects: z.number().int().gte(0).default(1), //number of target objects
+  input: z.object({
+    object: z.array(z.string()), // Object UIDs
+    distractor: z.array(z.string()), // Distractor UIDs
+    bg: z.array(z.string()), // Background UIDs
+  }),
 
-  // DEPTH OUTPUT (not tested)
-  outputDepth: z.boolean().default(false),
-  depthOutputDepth: z.number().int().default(16),
+  output: z.object({
+    images: z.number().int().gt(0), // Anzahl ausgegebener Bilder
+    just_merge: z.number().gte(0).lte(1), // Proportion von Bildern die nur durch Merging erzeugt werden
+    "skew_angle:material": z.number().gte(0) // Verhältnis von Winkel-Samples zu Material-Samples
+  }),
 
-  // AUGMENTATION
-  // emission_min: z.number().default(1), // only for environment maps
-  // emission_max: z.number().default(8), // only for environment maps
-  // light_number_min: z.number().default(1), // only for background images
-  // light_number_max: z.number().default(3), // only for background images
-  // light_energymin: z.number().default(20), // only for background images
-  // light_energymax: z.number().default(80), // only for background images
-  randomHSVValue: z.boolean().default(false), // randomize the value of HSV color space of the object with p=0.5
-  randomMetallicValue: z.boolean().default(false), //randomize the metallic object value with p=0.5
-  randomRoughnessValue: z.boolean().default(false), //randomize the roughness object value with p=0.5
+  render: z.object({
 
-  random_color: z.nativeEnum(RandomColor), //choose "None", "temperature", "projector"
+    camera: z.object({
+      lens_unit: z.nativeEnum(CamLensUnit),
+      lens: z.number().gt(0), // °|mm
+      sensor_height: z.number().gt(0), // mm
+      sensor_width: z.number().gt(0), // mm
+      clip_start: z.number().gte(0).default(0.1), // reine Performance, muss man nicht unbedingt einstellen können
+      clip_end: z.number().gte(0).default(50) // nur für sehr entfernte Objekte (= gar nicht) sinnvoll
+    }),
+    
+    resolution_x: z.number().int().gt(0).default(640), // px
+    resolution_y: z.number().int().gt(0).default(360), // px
 
-  // OBJECT COLOR (for PLY Files)
-  modelScale: z.number().gt(0).default(1), // model scale for PLY objects
-  hsv_hue: z.number().gte(0).lte(1).default(0.5), // changes hue of Hue Saturation Value Node, default 0.5
-  hsv_saturation: z.number().gte(0).lte(1).default(1), // changes saturation of Hue Saturation Value Node, default 1
-  hsv_value: z.number().gte(0).lte(1).default(1), // 0.35 // changes value of Hue Saturation Value Node, default 1
-  //roughness: z.number().gte(0).lte(1).default(0.3) //0.1 # Object Material Roughness (0=Mirror, 1=No Reflections)
+    model_scale: z.number().gt(0).default(1), //fragwürdig
+    exposure: z.number().gte(0).default(40),
+    compute_bbox: z.nativeEnum(ComputeBbox).default(ComputeBbox.FAST),
+    use_fps_keypoints: z.boolean().default(false), //muss nicht unbedgingt user-einstellbar sein
+    
+    use_cycles: z.boolean().default(true), // sollte immer true sein
+    samples: z.number().int().gte(10).lte(60).default(60), // sinnvolle obere Grenze ca. 60, unter 10 sinnlos
+    use_cycles_donoising: z.boolean().default(false), // sollte erstmal immer false sein
+    use_adaptive_sampling: z.boolean().default(false), // sollte erstmal immer false sein
+    use_GPU: z.boolean().default(true) // sollte immer true sein
+  }),
 
-  // camera sphere coordinates
-  camRMin: z.number().default(0.3), // minimum camera distance
-  camRMax: z.number().default(1.1), // maximum camera distance
-  camIncMin: z.number().default(0),
-  camIncMax: z.number().default(Math.PI / 2), // pi*2/3
-  camAziMin: z.number().default(0),
-  camAziMax: z.number().default(2 * Math.PI),
+  random: z.object({ // Config nimmt entweder [a,b] range oder konstante Zahl. Für b<=a nimmt die Übersetzerfunktion a als konstante Zahl.
+    min_distractors: z.number().int().gte(0),
+    max_distractors: z.number().int().gte(0),
 
-  //  OBJECT POSITION
-  objLocationXMin: z.number().default(-0.2), // translation in meters
-  objLocationXMax: z.number().default(0.2),
-  objLocationYMin: z.number().default(-0.2),
-  objLocationYMax: z.number().default(0.2),
-  objLocationZMin: z.number().default(-0.2),
-  objLocationZMax: z.number().default(0.2),
-  camRotationMin: z.number().default(0),
-  camRotationMax: z.number().default(2 * Math.PI),
+    min_x_pos: z.number(),
+    max_x_pos: z.number(),
+    min_y_pos: z.number(),
+    max_y_pos: z.number(),
+    min_z_pos: z.number(),
+    max_z_pos: z.number(),
 
-  maxBoundingBox: z.number().gte(0).lte(1).default(0.1), // filter out objects with bbox < -x or > 1+x (a value of 0.1 means max. 10% occlusion)
+    min_inc: z.number(),
+    max_inc: z.number(),
+    min_azi: z.number(),
+    max_azi: z.number(),
+  
+    min_metallic: z.number().gte(0).lte(1),
+    max_metallic: z.number().gte(0).lte(1),
+    min_roughness: z.number().gte(0).lte(1),
+    max_roughness: z.number().gte(0).lte(1),
+  }),
 
-  // Camera
-  camLensUnit: z.nativeEnum(CamLensUnit), // Choose 'FOV' or 'MILLIMETERS'
-  camLens: z.number().default(4.7), // Camera lens value in mm
-  camFov: z.number().default((59 + 90) / 2), // camera field of view in degrees
-  camSensorHeight: z.number().gt(0).default(3.84), // mm
-  camSensorWidth: z.number().gt(0).default(5.11), // mm
-
-  clipEnd: z.number().default(50),
-  clipStart: z.number().default(0.01),
-
-  //  RENDERING CONFIG
-  useGPU: z.boolean().default(true),
-  useCycles: z.boolean().default(true), // cycles or eevee
-  useCyclesDenoising: z.boolean().default(false),
-  useAdaptiveSampling: z.boolean().default(true),
-  resolutionX: z.number().gt(0).default(640), // pixel resolution
-  resolutionY: z.number().gt(0).default(360), // pixel resolution
-  samples: z.number().gt(0).default(512),
-
-  //  OUTPUT
-  numberOfRenders: z.number().default(20), // how many rendered examples
 });
 
 export type BlenderConfiguration = z.infer<typeof BlenderConfigurationObject>;
