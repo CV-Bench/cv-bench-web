300b86728641f77ee7cf18b59df277450d62a8df
Vova Shcherbyna <volodymyr.shcherbyna@campus.tu-berlin.de>
2023-03-03 11:15:30 +0100

    add rest of task API
    Signed-off-by: Vova Shcherbyna <volodymyr.shcherbyna@campus.tu-berlin.de>

--- apps/backend/routes/task/getTask.ts
+++ apps/backend/routes/task/getTask.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
 
-const getTask = (req: Request, res: Response) => {};
+import Database from "../../connectors/mongo";
+
+const getTask = (req: Request, res: Response) => {
+  Database.Task.findOne(req.params.id, req.session.user?._id)
+    .then(
+      (task) => {
+        res.status(200).json(task).send();
+      },
+      () => {
+        res.status(404).end();
+      }
+    )
+    .catch(() => {
+      res.status(500).end();
+    });
+};
 
 export default getTask;
