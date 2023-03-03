7be366174d8e378fb7abef07be8273cc5ffe72b6
Aron <45713863+fukuimaunicorn@users.noreply.github.com>
2023-03-03 18:23:50 +0100

    nvm

--- apps/backend/routes/task/getTask.ts
+++ apps/backend/routes/task/getTask.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
+import { redisClient } from "../../connectors/redis";
+
 import { Socket } from "../../connectors/socket";
 
 const getTask = (req: Request, res: Response) => {
-    Socket.Task.get(req.params.id).then(taskData => {
-        res.status(200).send(taskData);
-    }).catch(() => {
-        res.status(500).end();
-    })
+//   const taskId = req.params.id;
+
+//   redisClient.get(`taskLog:${taskId}`).then(result => {
+//     if(!result){
+//         // task log not available
+//         // so request from server and defer sending to socket
+//         Socket.Task.get(taskId);
+//         res.status(404).end();
+//     }
+//     else {
+//         res.status(200).send(result);
+//     }
+//   });
 };
 
 export default getTask;
