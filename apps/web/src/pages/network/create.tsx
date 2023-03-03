b3e5a39144aadd8f806b9d3d7c6099636dd26169
Kadi-7 <33463794+Kadi-7@users.noreply.github.com>
2023-03-03 18:27:50 +0100

    add fetch data and post data

--- apps/web/src/pages/network/create.tsx
+++ apps/web/src/pages/network/create.tsx
@@ -4,8 +4,12 @@ import TrainStep from "@/components/model/network/TrainStep";
 import DataStep from "@/components/model/network/DataStep";
 import ArchitectureStep from "@/components/model/network/ArchitectureStep";
 import { useState } from 'react';
-import { AccessType, PostNetwork, DatasetBody } from "types";
+import { AccessType, PostNetwork } from "types";
 import { title } from "process";
+import { useDatasetList } from "@/hooks/dataset";
+import { useArchtitucreList } from "@/hooks/archticture"; // not sure
+import { api } from "@/network";
+import { Router, useRouter } from "next/router";
 
 
 const CreateNetwork = () => {
@@ -37,20 +41,21 @@ const CreateNetwork = () => {
         setFormData({ ...formData, networkArchitectureId: id });
     };
 
+    const { push } = useRouter();
+
     const handleUpload = () => {
-        console.log(formData)
+        const postObject: PostNetwork = {
+            ...formData
+        };
     }
 
-    const data_data = [
-        {_id: 1, name: "TEst1", createdAt: "Test22", domainTags: ["11", "22", "33"], models: ["22", "22", 5]}, 
-        {_id: 2, name: "TEst2", createdAt: "Test2", domainTags: ["aa", "bb", "cc"], models: ["a", "b", 2]}, 
+    const { data_data } = useDatasetList();
+    const { arch_data } = useArchtitucreList();
 
-    ];
 
-    const arch_data = [
-        {_id: 1, name: "TEst1", createdAt: "Test22", requiredDatasetFormat: "COCO"}, 
-        {_id: 2, name: "TEst2", createdAt: "Test2", requiredDatasetFormat: "COCO"}, 
-    ];
+    //const data_data = [{_id: 1, name: "TEst1", createdAt: "Test22", domainTags: ["11", "22", "33"], models: ["22", "22", 5]}, {_id: 2, name: "TEst2", createdAt: "Test2", domainTags: ["aa", "bb", "cc"], models: ["a", "b", 2]}, ];
+
+    //const arch_data = [{_id: 1, name: "TEst1", createdAt: "Test22", requiredDatasetFormat: "COCO"}, {_id: 2, name: "TEst2", createdAt: "Test2", requiredDatasetFormat: "COCO"}, ];
 
     const data_columns = [
         { key: 'name', title: 'Name' },
