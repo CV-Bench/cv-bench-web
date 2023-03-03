e740764736adb8f88e3c3682f44ef2d47c9d2ce5
Kadi-7 <33463794+Kadi-7@users.noreply.github.com>
2023-03-03 04:31:50 +0100

    Update datasetep

--- apps/web/src/components/model/network/DataStep.tsx
+++ apps/web/src/components/model/network/DataStep.tsx
@@ -1,15 +1,31 @@
-import NetworkTable from "@/components/NetworkTable";
+import NetworkTableMultiple from "@/components/NetworkTableMultiple";
 import Card from "@/components/Card";
+import Description from "@/components/DescriptionComponent";
 
-const DataStep = (props) => {
+interface DataStepProps {
+    datasetId: number[],
+    columns: { key: string, title: string }[],
+    data: { [key: string]: any }[],
+    onSelectDataset: (datasetId?: number[]) => void,
+  }
+  
+const DataStep = (props: DataStepProps) => {
     return (<>
+
         <div className="flex w-full h-full">
             <Card className="lg:w-1/2 h-full m-2">
-                <NetworkTable
+                <Description
+                    title="Dataset selection"
+                    description="please select one or more data sets to train on them"
+                    imageUrl="https://cdn-icons-png.flaticon.com/512/250/250235.png?w=1380&t=st=1677803698~exp=1677804298~hmac=71898ce48517582048decfbbc3b05c4c80ad3e59d513bbccea4a922801f3eb0e"
+                />
+                <NetworkTableMultiple
                     data={props.data}
                     columns={props.columns}
-                    selectedRows={props.selectedRows}
-                    setSelectedRows={props.setSelectedRows} />
+                    datasetId={props.datasetId}
+                    onSelect={props.onSelectDataset}
+                    />
+
             </Card>
             <Card className="lg:w-1/2 h-full m-2">
                 DESC
