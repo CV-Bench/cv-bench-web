0aa04218f9fad8b8f311d4eb75c5b5fb25e6dd5e
Aron <45713863+fukuimaunicorn@users.noreply.github.com>
2023-03-03 17:06:26 +0100

    properly implements s3 download

--- apps/backend/routes/download.ts
+++ apps/backend/routes/download.ts
@@ -1,19 +1,70 @@
 import { Request, Response } from "express";
 
-import { Bucket, DataType } from "shared-types";
+import {
+  Bucket,
+  DatasetDb,
+  DataType,
+  loggerTitle,
+  ModelDb
+} from "shared-types";
 
+import Database from "../connectors/mongo";
 import { getPresignedUrl } from "../connectors/s3";
 import { Socket } from "../connectors/socket";
+import logger from "../util/logger";
 
 const download = (req: Request, res: Response) => {
-  getPresignedUrl(req.params.bucket as Bucket, req.params.key)
-    .then((url) => {
+  const dataType: DataType = req.params.type as DataType;
+  const dataId = req.params.id;
+  const user = req.session.user;
+
+  // generate presigned url for client to download resource
+  const sendDownloadLink = (key: string) => {
+    getPresignedUrl(key).then((url) => {
       res.status(200).send(url);
-    })
-    .catch(() => {
-      Socket.Data.upload(req.params.dataId, req.params.dataType as DataType);
-      res.status(200).end();
     });
+  };
+
+  // ask server to upload data
+  const requestDataUpload = () => {
+    Socket.Data.upload(dataId, dataType);
+  };
+
+  // if no key request data upload, else return presigned url
+  const processDbResult = (result: ModelDb | DatasetDb) => {
+    if (!result.s3Key) {
+      requestDataUpload();
+      res.status(404).send("Content Unavailable");
+    } else sendDownloadLink(result.s3Key);
+  };
+
+  const processDbError = (e: any) => {
+    logger.error(
+      loggerTitle.MONGO_CLIENT,
+      "Express Route Download",
+      "Accessing MongoDB failed",
+      e
+    );
+  };
+
+  // if undefined => bad request
+  if (!dataId || !dataType) res.status(400).end();
+  else {
+    switch (dataType) {
+      case DataType.DATASET:
+        Database.Dataset.findOne(dataId, user?._id)
+          .then(processDbResult)
+          .catch(processDbError);
+        break;
+      case DataType.MODEL:
+        Database.Model.findOne(dataId, user?._id)
+          .then(processDbResult)
+          .catch(processDbError);
+        break;
+      default:
+        res.status(400).end();
+    }
+  }
 };
 
 export default download;
