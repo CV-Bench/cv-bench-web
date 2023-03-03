df72a3e562b5baee26ae94a2dbc3e35837e5701d
Kadi-7 <33463794+Kadi-7@users.noreply.github.com>
2023-03-03 04:32:24 +0100

    update create

--- apps/web/src/pages/network/create.tsx
+++ apps/web/src/pages/network/create.tsx
@@ -4,17 +4,24 @@ import FormStepsPanel, {
 import * as z from "zod";
 import TrainStep from "@/components/model/network/TrainStep";
 import DataStep from "@/components/model/network/DataStep";
+import ArchitectureStep from "@/components/model/network/ArchitectureStep";
 import { useState } from 'react';
 import { AccessType, PostNetwork } from "types";
 
 
 const CreateNetwork = () => {
     const [formData, setFormData] = useState<
-        Pick<PostNetwork, "accessType" | "domainTags">
+        Pick<PostNetwork, "accessType" | "domainTags" | "name" | "datasetId" | "networkArchitectureId">
     >({
         accessType: AccessType.PUBLIC,
         domainTags: [],
+        name: "",
+        datasetId: []
     });
+
+
+    const onSetName = (val: string) =>
+        setFormData({ ...formData, name: val });
 
     const onSelectTags = (val: string[]) =>
         setFormData({ ...formData, domainTags: val });
@@ -22,45 +29,70 @@ const CreateNetwork = () => {
     const onSelectAccessType = (val: AccessType) =>
         setFormData({ ...formData, accessType: val });
 
+    const onSelectDataset = (id: number[] | undefined) => {
+        setFormData({ ...formData, datasetId: id });
+    };
 
-    const [selectedRows, setSelectedRows] = useState([]);
-
-
-    const check = () => {
-        console.log(formData)
-
-    }
+    const onSelectNetworkArchitecture = (id: number| undefined) => {
+        setFormData({ ...formData, networkArchitectureId: id });
+    };
 
     const handleUpload = () => {
-        console.log("UPLOAD")
+        console.log(formData)
     }
 
-    const data = [
+    const data_data = [
         { id: 1, name: 'A', age: 25, jj: 12 },
         { id: 2, name: 'B', age: 30, k: 14 },
         { id: 3, name: 'C', age: 35, ee: 12 },
     ];
 
-    const columns = [
+    const columns_data = [
         { key: 'id', title: 'ID' },
         { key: 'name', title: 'Name' },
         { key: 'age', title: 'Age' }
     ];
 
+    const data_arch = [
+        { id: 1, name: 'A', age: 25, jj: 12 },
+        { id: 2, name: 'B', age: 30, k: 14 },
+        { id: 3, name: 'C', age: 35, ee: 12 },
+    ];
+
+    const columns_arch = [
+        { key: 'id', title: 'ID' },
+        { key: 'name', title: 'Name' },
+        { key: 'age', title: 'Age' }
+    ];
     const steps: FormStep[] = [
         {
             name: "Dataset",
             description: "tbd",
             component: (
                 <DataStep
-                    selectedRows={selectedRows}
-                    setSelectedRows={setSelectedRows}
-                    columns={columns}
-                    data={data}
+                    datasetId={formData.datasetId}
+                    onSelectDataset={onSelectDataset}
+                    columns={columns_data}
+                    data={data_data}
                 />
             ),
             validation: z.object({
-                selectedRows: z.array(z.number()).min(1),
+                datasetId: z.array(z.number()).min(1),
+            })
+        },
+        {
+            name: "Architecture",
+            description: "tbd",
+            component: (
+                <ArchitectureStep
+                    networkArchitectureId={formData.networkArchitectureId}
+                    onSelectNetworkArchitecture={onSelectNetworkArchitecture}
+                    columns={columns_arch}
+                    data={data_arch}
+                />
+            ),
+            validation: z.object({
+                networkArchitectureId: z.number(),
             })
         },
         {
@@ -72,20 +104,22 @@ const CreateNetwork = () => {
                     onSelectTags={onSelectTags}
                     accessType={formData.accessType}
                     onSelectAccessType={onSelectAccessType}
+                    name={formData.name}
+                    onSetName={onSetName}
 
                 />
             ),
-            validation: z.object({})
+            validation: z.object({
+                name: z.string().min(1),
+
+            })
         }
     ];
     return (<>
         <div className="">
-            <button onClick={check}>
-                CHECK
-            </button>
             <FormStepsPanel
                 submitButtonText="Start Training"
-                formData={{ selectedRows }}
+                formData={formData}
                 steps={steps}
                 handleSubmit={handleUpload}
             />
