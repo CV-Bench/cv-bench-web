2936014fecca2e19f566897ef708c805df556a89
ReykCS <reyk-carstens@web.de>
2023-03-05 22:49:58 +0100

    new design for backgrounds list

--- apps/web/src/pages/background/index.tsx
+++ apps/web/src/pages/background/index.tsx
@@ -15,27 +15,31 @@ const BackgroundList: React.FC = () => {
   }
 
   return (
-    <div className="container mx-auto py-8 space-y-4">
-      <Card>
-        <div className="p-4 flex space-x-4">
-          <div>
-            <div className="w-12 h-12 flex items-center justify-center text-slate-600">
-              <PhotoIcon className="w-full h-full" />
+    <div className="grid grid-cols-4 text-white container mx-auto py-8 divide-x divide-slate-600 relative">
+      <div className="pr-2 relative">
+        <Card className="sticky top-8">
+          <div className="p-4 flex space-x-4">
+            <div>
+              <div className="w-12 h-12 flex items-center justify-center text-slate-600">
+                <PhotoIcon className="w-full h-full" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-4">
+              <div className="space-y-1">
+                <p className="font-medium text-lg text-slate-200">
+                  Backgrounds
+                </p>
+                <p className="text-sm text-slate-400"></p>
+              </div>
+              <Link href="/background/upload">
+                <Button>Upload Background</Button>
+              </Link>
             </div>
           </div>
-          <div className="flex flex-col space-y-4">
-            <div className="space-y-1">
-              <p className="font-medium text-lg text-slate-200">Backgrounds</p>
-              <p className="text-sm text-slate-400"></p>
-            </div>
-            <Link href="/background/upload">
-              <Button>Upload Background</Button>
-            </Link>
-          </div>
-        </div>
-      </Card>
+        </Card>
+      </div>
 
-      <div className="grid lg:grid-cols-5 sm:grid-cols-1 gap-4">
+      <div className="grid grid-cols-3 gap-4 col-span-3 pl-2">
         {backgrounds.map((background, i) => (
           <ImageCard
             {...background}
