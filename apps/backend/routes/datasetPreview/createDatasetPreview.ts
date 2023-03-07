e7dbdfdb32b7030ed1705481bb95d89707aa3304
ReykCS <reyk-carstens@web.de>
2023-03-07 23:25:35 +0100

    fix dataset preview endpoint

--- apps/backend/routes/datasetPreview/createDatasetPreview.ts
+++ apps/backend/routes/datasetPreview/createDatasetPreview.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { ObjectId } from "mongodb";
 
 import { PostDatasetPreview, TypedRequest } from "shared-types";
 
@@ -8,8 +9,9 @@ const createDatasetConfiguration = (
   req: TypedRequest<PostDatasetPreview>,
   res: Response
 ) => {
-  const preview = req.body as PostDatasetPreview;
-  Database.DatasetPreview.insert(preview)
+  const { taskId, image } = req.body as PostDatasetPreview;
+
+  Database.DatasetPreview.insertOne({ taskId: new ObjectId(taskId), image })
     .then((value) => res.json(value.insertedId).end())
     .catch((x) => res.status(500).end());
 };
