41b5b1377d77fc742c09f1ae9adf645953115c31
ReykCS <reyk-carstens@web.de>
2023-03-09 17:31:29 +0100

    removed weird merge relicts

--- apps/backend/routes/network/deleteNetwork.ts
+++ apps/backend/routes/network/deleteNetwork.ts
@@ -1,23 +1,5 @@
 import { Request, Response } from "express";
-import { DataType } from "shared-types";
-import Database from "../../connectors/mongo";
-import S3 from "../../connectors/s3";
-import { Socket } from "../../connectors/socket";
 
-<<<<<<< HEAD
-const deleteNetwork = (req: Request, res: Response) => {
-    Database.Network.findOne(req.params.id, req.session.user?._id)
-    .then(network => {
-        const actions: Promise<any>[] = [];
-        if (network.s3Key) {
-            actions.push(S3.Network.delete(network.s3Key));
-        }
-        actions.push(Database.Network.deleteOne(req.params.id, req.session.user?._id));
-
-        Socket.Data.deleteData(req.params.id, DataType.NETWORK);
-
-        Promise.all(actions).then(() => res.status(200));
-=======
 import { DataType } from "shared-types";
 
 import Database from "../../connectors/mongo";
@@ -38,7 +20,6 @@ const deleteNetwork = (req: Request, res: Response) => {
       Socket.Data.deleteData(req.params.id, DataType.NETWORK);
 
       Promise.all(actions).then(() => res.status(200));
->>>>>>> main
     })
     .catch(() => res.status(500));
 };
