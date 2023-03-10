8b52a7f6ada7daf4765d215ceffa4e5732a89a0f
Kolja Kieslich <kolja.kieslich@gmail.com>
2023-03-10 15:37:57 +0100

    Update network & Update dataset endpoints

--- apps/backend/routes/dataset/updateDataset.ts
+++ apps/backend/routes/dataset/updateDataset.ts
@@ -1,7 +1,20 @@
 import { Response } from "express";
 
 import { PatchDataset, TypedRequest } from "shared-types";
+import Database from "../../connectors/mongo";
 
-const updateDataset = (req: TypedRequest<PatchDataset>, res: Response) => {};
+const updateDataset = (req: TypedRequest<PatchDataset>, res: Response) => {
+  Database.Dataset.updateOne(req.params.id, req.session.user?._id, req.body)
+    .then((result) => {
+      if (result.matchedCount < 1) {
+        return res.status(404).end();
+      }
+
+      res.status(200).json({});
+    })
+    .catch(() => {
+      res.status(500).end();
+    });
+};
 
 export default updateDataset;
--- apps/backend/routes/network/updateNetwork.ts
+++ apps/backend/routes/network/updateNetwork.ts
@@ -1,7 +1,20 @@
 import { Response } from "express";
 
 import { PatchNetwork, TypedRequest } from "shared-types";
+import Database from "../../connectors/mongo";
 
-const updateNetwork = (req: TypedRequest<PatchNetwork>, res: Response) => {};
+const updateNetwork = (req: TypedRequest<PatchNetwork>, res: Response) => {
+  Database.Network.updateOne(req.params.id, req.session.user?._id, req.body)
+  .then((result) => {
+    if (result.matchedCount < 1) {
+      return res.status(404).end();
+    }
+
+    res.status(200).json({});
+  })
+  .catch(() => {
+    res.status(500).end();
+  });
+};
 
 export default updateNetwork;
